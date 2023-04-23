8547ad8f3b263f40022b0f615533290c96eb3d37
bekatadesse <bekatadesse5@gmail.com>
2023-04-23 13:30:49 +0300

    view index page

--- chat_app/chat_app/settings.py
+++ chat_app/chat_app/settings.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'channels',
+    'core',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -68,6 +71,7 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'chat_app.wsgi.application'
+ASGI_APPLICATION = 'chat_app.asgi.application'
 
 
 # Database
