506b4fa31c7a4f381323bb8122dadc4110993e29
bekatadesse <bekatadesse5@gmail.com>
2023-04-23 16:22:45 +0300

    access room views

--- chat_app/room/views.py
+++ chat_app/room/views.py
@@ -9,3 +9,10 @@ def rooms(request):
 
     return render(request, 'room/rooms.html', {'rooms': rooms})
 
+
+@login_required
+def room(request, slug):
+    room = Room.objects.get(slug=slug)
+    # messages = Message.objects.filter(room=room)[0:25]
+
+    return render(request, 'room/room.html', {'room': room })
